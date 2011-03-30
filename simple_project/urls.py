d333e1a6eb344c7be35e67dcec42e200442c84b7
Darii Denis Home <denis.darii@gmail.com>
2011-03-30 23:52:13 +0200

    changed urls.py, added index view and index.html template to core application

--- simple_project/urls.py
+++ simple_project/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    # enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    (r'^$', "core.views.index"),
 )
