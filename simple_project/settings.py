65c5a891ca192198af47e85439b4ddcfd5ec0b97
Darii Denis Home <denis.darii@gmail.com>
2011-03-30 21:40:40 +0200

    configured sqlite3 and created db/ folder for the database file

--- simple_project/settings.py
+++ simple_project/settings.py
@@ -32,8 +32,8 @@ SEND_BROKEN_LINK_EMAILS = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'db/simple-project.db'),   # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
