67920342b8f28359526bdc86028157af52c24e08
Darii Denis <denis.darii@gmail.com>
2011-04-10 20:26:09 +0200

    added user field in Message model and auto-fill functionality in MessageAdmin class

--- simple_project/apps/core/admin.py
+++ simple_project/apps/core/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from core.models import *
 
 class MessageAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        """
+        Automatically fill in the user field on creation.
+        """
+        if not change:
+            obj.user = request.user
+        super(self.__class__, self).save_model(request, obj, form, change)
 
 admin.site.register(Message, MessageAdmin)
--- simple_project/apps/core/models.py
+++ simple_project/apps/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Message(models.Model):
     message = models.TextField()
+    user = models.ForeignKey(User, editable=False)
 
     def __unicode__(self):
         return self.message
