0cd40036eacf553a801a8dc0993198dd721baa3c
Darii Denis <denis.darii@gmail.com>
2011-04-19 13:20:18 +0200

    added username_present def in utils.py

--- simple_project/apps/core/utils.py
+++ simple_project/apps/core/utils.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
+
 """
 Generate a random password.
 Code from http://code.activestate.com/recipes/59873-random-password-generation/
 """
 def gen_passwd(length=8, chars=string.letters + string.digits):
     return ''.join([choice(chars) for i in range(length)])
+
+def username_present(username):
+    if User.objects.filter(username=username).count():
+        return True
+    
+    return False
